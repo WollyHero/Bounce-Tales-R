c152e5e6ece8166fe546957f24a9c0ff9d0b724e
WollyHero <cobblerpeak@gmail.com>
2022-09-19 21:39:04 -0500

    Main Menu: Script
    Now the buttons works!

---
+++ Bounce Tales R/Assets/Scripts/Main Menu/MainMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+    
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Exit ()
+    {
+        Application.Quit();
+    }
+
+//El enlace posiblemente se modificará pronto.
+    public void MoreGames()
+    {
+        Application.OpenURL("https://bounceteam.org");
+    }
+}
