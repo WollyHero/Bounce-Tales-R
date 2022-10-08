bd4377172f61bb0cc5cac1331f11158b1ae2b9f5
DAVID <miltondavid1108@gmail.com>
2022-10-08 10:18:49 -0500

    Push Script seemingly done

--- Bounce Tales R/Assets/Scripts/PushBounce.cs
+++ Bounce Tales R/Assets/Scripts/PushBounce.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class PushBounce : MonoBehaviour
 {
-	[SerializeField]private Transform Bounce;
-	private void Update() {
-		print(GetPushDir());
+	[SerializeField]private GameObject Bounce;
+	[Range(0,20)][SerializeField]private float ForceToPush;
+	private Rigidbody2D rb;
+	private void Awake() {
+		if(Bounce != null){rb = Bounce.transform.GetComponent<Rigidbody2D>();};
 	}
-	private Vector3 GetPushDir(){
-		Vector3 PushDir = (transform.position - Bounce.position);
-		return PushDir;
+	private Vector2 GetPushDir(float force){
+		Vector3 PushDir = (Bounce.transform.position - transform.position);
+		float x = Mathf.Sign(PushDir.x)* force;
+		float y = Mathf.Sign(PushDir.y)* force;
+		return new Vector2(x,y);
+	}
+	private void OnCollisionEnter2D(Collision2D other) {
+		rb.AddForce(GetPushDir(ForceToPush), ForceMode2D.Impulse);
 	}
 }
