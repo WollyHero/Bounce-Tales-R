ef86bdb4acb0d2c94e6fddfc3a3bf38071cb18ca
DAVID <miltondavid1108@gmail.com>
2022-10-30 15:27:43 -0500

    fix main menu

--- Bounce Tales R/Assets/Scripts/Main Menu/CharacterSelec.cs
+++ Bounce Tales R/Assets/Scripts/Main Menu/CharacterSelec.cs
@@ -1,17 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class CharacterSelec : MonoBehaviour
 {
     [SerializeField] RectTransform[] MainMenuButtons;
     [SerializeField] RectTransform indicator;
     [SerializeField] float moveDelay;
-
+    [SerializeField] float Speed;
+    [SerializeField] Color colorChange;
     public int indicatorPos;
     public float moveTimer;
+    private List<Button> buttons = new List<Button>();
 
     // Update is called once per frame
+    private void Awake()
+    {
+        if (MainMenuButtons != null)
+        {
+            foreach (var item in MainMenuButtons)
+            {
+                buttons.Add(item.transform.GetComponent<Button>());
+            }
+        }
+    }
     void Update()
     {
         if (moveTimer <= moveDelay)
@@ -49,12 +63,12 @@ public class CharacterSelec : MonoBehaviour
                 moveTimer = 0;
             }
         }
-        indicator.localPosition = MainMenuButtons[indicatorPos].localPosition;
-
-        if (Input.GetKeyDown(KeyCode.Space))
+        indicator.localPosition = Vector3.Lerp(indicator.localPosition, MainMenuButtons[indicatorPos].localPosition, Time.deltaTime * Speed);
+        if (Input.GetKeyDown(KeyCode.Return))
         {
-            Debug.Log("Character " + (indicatorPos + 1));
+            ExecuteEvents.Execute<IPointerClickHandler>(buttons[indicatorPos].gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerClickHandler);
         }
+        UpdateColor(indicatorPos);
     }
 
     public void HoverOnButton(int btnPos)
@@ -64,6 +78,28 @@ public class CharacterSelec : MonoBehaviour
 
     public void ButtonPress()
     {
-        Debug.Log("Character " + (indicatorPos + 1));
+        Debug.Log("Character " + (indicatorPos));
+    }
+    private void UpdateColor(int index)
+    {
+        for (int i = 0; i < MainMenuButtons.Length; i++)
+        {
+
+            ColorBlock cb = buttons[i].colors;
+            if (i == index)
+            {
+                cb.normalColor = colorChange;
+                cb.selectedColor = colorChange;
+                cb.highlightedColor = colorChange;
+                buttons[i].colors = cb;
+            }
+            else
+            {
+                cb.normalColor = Color.white;
+                cb.selectedColor = Color.white;
+                cb.highlightedColor = Color.white;
+                buttons[i].colors = cb;
+            }
+        }
     }
 }
